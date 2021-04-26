a8e14dfc8130ac7b5c45b45788ba47f78b21ca6b
Pwako11 <akolo@hotmail.com>
2021-04-25 21:15:32 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  let titledTutorial = []  
+  let list = tutorials.map(function(tutorial) {
+    const words = tutorial.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1); 
+    }
+    titledTutorial.push(words.join(' ')); 
+      
+  });
+  return titledTutorial 
+};
+titleCased()
